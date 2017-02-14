4912a564c34c4a89453d82fd72de437fd31fa860
Damian Tomaszewski <damian.tomaszewski@gmail.com>
2017-02-14 11:20:51 +0000

    working version (I hope)

---
+++ info/urladd.js
@@ -0,0 +1,2 @@
+var myurl = window.location.href;
+document.getElementById('example').innerHTML = myurl+"December%2015,%202015<br/>"+myurl+"1450137600";
--- server.js
+++ server.js
@@ -1,10 +1,36 @@
-var express = require('express');
+var express = require('express'),
+    url = require('url'),
+    moment = require('moment');
+moment().format();
 var app = express();
+app.use('/',express.static('info'));
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
+app.get('/*', function (req, res) {
+  var dateString = url.parse(req.url,true);
+  var dateStringSlice = dateString.pathname.slice(1);
+  var result = {};
+  if(dateString.pathname!=='/') {
+     var m1 = moment(decodeURI(dateStringSlice));
+     var m2 = moment(Number(dateStringSlice));
+     if(m1.isValid() || m2.isValid()){
+         if(m1.isValid()){
+             result.unix = Number(m1.format('X'));
+             result.natural = m1.format('MMMM')+' '+m1.format('DD')+', '+m1.format('YYYY');
+         }
+         else{
+             result.unix = Number(m2.format('X'));
+             result.natural = m2.format('MMMM')+' '+m2.format('DD')+', '+m2.format('YYYY');
+         }
+         
+     }else
+     {
+        result.unix = null;
+        result.natural = null; 
+     }
+   res.send(result);
+  }
 });
 
-app.listen(8080, function () {
-  console.log('Example app listening on port 3000!')
-});
+
+
+app.listen(8080);
